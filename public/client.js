a5e6d40218ddca1343c8d47f887193973bed688b
unknown <alexperrakis1@gmail.com>
2020-01-21 11:49:00 +0200

    - Fixed but with width not calculating correct with inline-flex

--- public/client.js
+++ public/client.js
@@ -11,43 +11,41 @@ socket.on('play_cards', (data)=> {
         played_cards[i].style.display = "none";
         //played_cards[i].classList.add("fade-out");
     }
+    
+    let inDiv = document.getElementsByClassName("inDiv");    
+    inDiv[0].innerHTML = "";
+;
+    //outDiv.className = "played_cards";
 
-
     
-    let frame = document.getElementsByClassName("frame");
-    
-    let outDiv = document.createElement("DIV");
-    let inDiv = document.createElement("DIV");
-    
-    
-    outDiv.className = "played_cards";
-
-    let width_value = 45 + 45 * (data.length);
-    
-
-    inDiv.style = "display: none;";
+    //inDiv.style = "display: none;";
 
     let left_value;
+    let save_cards = [];
     for(let i = 0; i < data.length; i++){
         //let left = 302.5 + (45 * i);
-        left_value = -45 * i;
+        left_value = (-45 * i);
+        left_value += (22.5 * (data.length -1));
         let imgCard = document.createElement("IMG");
+        save_cards.push(imgCard);
         imgCard.src = "images/deck/" + data[i].suit + "/" + data[i].value  +  ".png";
-        
-
-        inDiv.appendChild(imgCard);  
-        imgCard.style = "display: inline-block; border-radius: 9px; border-style: solid; border-width: 1px; width: 88px; height: 139px; position: relative; left: " + left_value + "px;";
-
+          
+       // imgCard.style = "display: inline-block; border-radius: 9px; border-style: solid; border-width: 1px; width: 88px; height: 139px; position: relative; left: " + left_value + "px;";
+        imgCard.classList.add("imgCard");
+        imgCard.style.left = left_value + "px";
+        imgCard.style.width = "88px";
+        inDiv[0].appendChild(imgCard);
         
     }
 
-    outDiv.appendChild(inDiv);
-    frame[0].appendChild(outDiv);
-    
+
+
     setTimeout(()=> {   
-        outDiv.style = "width: 100%; height: 139px; position: absolute; top: 305px;";
-        inDiv.style = "display: flex; width: " + width_value + "px; height: 139px; margin: auto";
-    }, 0);
+        for(let i = 0; i < save_cards.length; i++){
+            //save_cards[i].style.left += ;
+        }
+        
+    }, 1000); 
 
 
 });
